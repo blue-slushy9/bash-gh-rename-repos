5a1da2848285fefa983598b471084423c6111858
blue-slushy9 <ra.cruz971@gmail.com>
2024-01-20 13:38:10 -0800

    figured out the trim stuff, onto my second function now

--- rename_gh_repos.sh
+++ rename_gh_repos.sh
@@ -6,29 +6,49 @@
 # account via GitHub CLI, I do not need to specify the user;
 # We use ">" to export the output to a text file for later use,
 # which will go in our working directory (the local repo of this script);
-gh repo list -L 110 > ./gh_repos1.txt
+# The awk command ensures only the first column of the table is output,
+# we do not need the repo metadata, only the names;
+gh repo list -L 110 | awk '{print $1}' > ./gh_repos1.txt
 
 # Next we go through each line in our text file and see if it contains ".py",
 # as it is primarily Python repos which I will be renaming;
 
 # Specify filepath of our list of repos;
-repolist="./gh_repos1.txt"
+repolist1="./gh_repos1.txt"
 
 # We will look for this substring in each line (i.e. repo name), because all of
 # of my Python repo names used to end in ".py";
 substring=".py"
 
 # Verify our repolist exists;
-if [ -f "$repolist" ]; then
+if [ -f "$repolist1" ]; then
     # Open the file and read it line by line, until source is exhausted;
     while IFS= read -r line; do
         # Look for our substring inside of every line (i.e. repo name);
         if [[ $line == *"$substring"* ]]; then
-            # Trim the trailing whitespaces, etc. to leave only "blue-slushy9/<repo-name>";
-            trim_line="${line%"${line##*[![:space:]]}"}"
-            # DEBUG
+            # Trim anything before the "/", inclusive;
+            trim_line="${line#*/}"
+            # Then trim everything after the ".py";
+            #echo "${line%%*(.py)}"
+            #trim_line="${line%*(.py)}"
+            
+            # Output the trimmed line (i.e. repo name) to our final list;
             echo "$trim_line" >> gh_repos2.txt
         fi
     # Redirects the input of the while loop to come from a specified file, e.g. our repolist;    
-    done < "$repolist"
+    done < "$repolist1"
 fi
+
+# We need to assign out new repolist to a variable to use it in our next loop;
+repolist2="./gh_repos2.txt"
+
+# Next we loop through each line (i.e. repo name) in our second list, and we insert the repo name
+# into our GitHub CLI command that will rename it into the standard format I have developed;
+command="gh repo rename -R blue-slushy9/$old_name $new_name"
+
+# Verify our second repolist exists;
+if [ -f "$repolist2" ]; then
+    # Open the file and read it line by line, until source is exhausted;
+    while IFS= read -r line; do
+        old_name="$line"
+        new_name=
